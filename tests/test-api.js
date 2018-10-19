4e9f2d6c7f6455696407974f3b310b770ad29b41
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2018-10-19 12:02:30 +0200

    added index.js

--- tests/test-api.js
+++ tests/test-api.js
@@ -1,5 +1,4 @@
-import AudioContext from '../src/AudioContext';
-import AudioBufferLoader from '../src/AudioBufferLoader';
+import { AudioContext, AudioBufferLoader } from '../src/';
 import * as masters from 'waves-masters';
 import path from 'path';
 
@@ -8,62 +7,62 @@ const audioContext = new AudioContext();
 
 // AM, FM, scheduling, linearRamp
 
-// const scheduler = new masters.Scheduler(() => {
-//   return audioContext.currentTime;
-// });
+const scheduler = new masters.Scheduler(() => {
+  return audioContext.currentTime;
+});
 
-// class Engine extends masters.TimeEngine {
-//   constructor() {
-//     super();
+class Engine extends masters.TimeEngine {
+  constructor() {
+    super();
 
-//     const ramp = audioContext.createGain();
-//     ramp.gain.value = 0;
-//     ramp.connect(audioContext.destination);
+    const ramp = audioContext.createGain();
+    ramp.gain.value = 0;
+    ramp.connect(audioContext.destination);
 
-//     const env = audioContext.createGain();
-//     env.gain.value = 0.7;
-//     env.connect(ramp);
+    const env = audioContext.createGain();
+    env.gain.value = 0.7;
+    env.connect(ramp);
 
-//     const osc = audioContext.createOscillator();
-//     osc.frequency.value = 400;
-//     osc.connect(env);
+    const osc = audioContext.createOscillator();
+    osc.frequency.value = 400;
+    osc.connect(env);
 
-//     // AM
-//     const modAmpScale = audioContext.createGain();
-//     modAmpScale.gain.value = 0.3;
-//     modAmpScale.connect(env.gain);
+    // AM
+    const modAmpScale = audioContext.createGain();
+    modAmpScale.gain.value = 0.3;
+    modAmpScale.connect(env.gain);
 
-//     const modAmp = audioContext.createOscillator();
-//     modAmp.frequency.value = 25;
-//     modAmp.connect(modAmpScale);
+    const modAmp = audioContext.createOscillator();
+    modAmp.frequency.value = 25;
+    modAmp.connect(modAmpScale);
 
-//     // FM
-//     const modFreq = audioContext.createOscillator();
-//     modFreq.frequency.value = 40;
-//     modFreq.connect(osc.frequency);
+    // FM
+    const modFreq = audioContext.createOscillator();
+    modFreq.frequency.value = 40;
+    modFreq.connect(osc.frequency);
 
-//     osc.start(0);
-//     modAmp.start(0);
-//     modFreq.start(0);
+    osc.start(0);
+    modAmp.start(0);
+    modFreq.start(0);
 
-//     this.ramp = ramp;
-//     this.period = 1;
-//   }
+    this.ramp = ramp;
+    this.period = 1;
+  }
 
-//   advanceTime(time) {
+  advanceTime(time) {
 
-//     // problems w/ ramps
-//     const now = time;
-//     this.ramp.gain.setValueAtTime(0, now);
-//     this.ramp.gain.linearRampToValueAtTime(1, now + 0.01);
-//     this.ramp.gain.exponentialRampToValueAtTime(0.001, now + 1);
+    // problems w/ ramps
+    const now = time;
+    this.ramp.gain.setValueAtTime(0, now);
+    this.ramp.gain.linearRampToValueAtTime(1, now + 0.01);
+    this.ramp.gain.exponentialRampToValueAtTime(0.001, now + 1);
 
-//     return time + this.period;
-//   }
-// }
+    return time + this.period;
+  }
+}
 
-// const engine = new Engine();
-// scheduler.add(engine);
+const engine = new Engine();
+scheduler.add(engine);
 
 // start / stop
 // const osc = audioContext.createOscillator();
@@ -86,17 +85,17 @@ const audioContext = new AudioContext();
 // }());
 
 
-const loader = new AudioBufferLoader();
+// const loader = new AudioBufferLoader();
 
-loader
-  .load(path.join(__dirname, 'audio', 'voice.wav'))
-  .then(buffer => {
-    const src = audioContext.createBufferSource();
-    src.buffer = buffer;
-    src.connect(audioContext.destination);
-    src.start(audioContext.currentTime);
-  })
-  .catch(err => console.error(err));
+// loader
+//   .load(path.join(__dirname, 'audio', 'voice.wav'))
+//   .then(buffer => {
+//     const src = audioContext.createBufferSource();
+//     src.buffer = buffer;
+//     src.connect(audioContext.destination);
+//     src.start(audioContext.currentTime);
+//   })
+//   .catch(err => console.error(err));
 
 
 
